d3ad78fe6215c636676f44a7de97a9cbdde4b0ba
Chad Granum <chad.granum@dreamhost.com>
2014-01-08 19:52:40 -0800

    Complete

--- lib/Test/FixtureBuilder.pm
+++ lib/Test/FixtureBuilder.pm
@@ -33,12 +33,6 @@ default_export fixture_db => sub {
         if exists $meta->{db};
     $meta->{db} = $name;
 
-    my $dbh = $meta->{class}->name_to_handle($name);
-    croak "Could not get db handle for '$name'"
-        unless $dbh;
-
-    $meta->{dbh} = $dbh;
-
     $meta->{builder} = $meta->{class}->new(%$meta);
 
     my $success = eval { $code->($meta->{builder}); 1 };
@@ -49,7 +43,7 @@ default_export fixture_db => sub {
     die $error || "Unknown Error"
         unless $success;
 
-    return $dbh;
+    return $name;
 };
 
 default_export fixture_table => sub {
@@ -114,32 +108,34 @@ sub new {
     return $self;
 }
 
-for my $field (qw/db dbh class table/) {
+sub db {
+    my $self = shift;
+
+    return $self->dbh
+        unless @_;
+
+    my ($name) = @_;
+
+    my $dbh = $self->name_to_handle($name);
+    croak "Could not get db handle for '$name'"
+        unless $dbh;
+
+    $self->dbh( $dbh );
+}
+
+for my $field (qw/dbh class/) {
     my $accessor = sub {
         my $self = shift;
 
         croak "Accessor '$field' called without instance!"
             unless blessed $self && $self->isa( __PACKAGE__ );
 
-        $self->{$field} = @_ if @_;
+        ($self->{$field}) = @_ if @_;
 
         return $self->{$field};
     };
     no strict 'refs';
     *$field = $accessor;
-}
-
-sub load {
-    my $self = shift;
-
-    croak "load() can only be used when the table is set"
-        unless $self->table;
-
-    for my $row (@_) {
-        $self->insert_row( $self->table, $row );
-    }
-
-    return;
 }
 
 sub insert_row {
@@ -150,7 +146,7 @@ sub insert_row {
 
     my $quoted_table = $dbh->quote_identifier(undef, undef, $table);
     my @vals = values %$row;
-    my $cols = join ',' => map { '`' . $dbh->quote_identifier($_) . '`' } keys %$row;
+    my $cols = join ',' => map { $dbh->quote_identifier($_) } keys %$row;
     my $vals = join ',' => map { '?' } @vals;
 
     my $sth = $dbh->prepare("INSERT INTO $quoted_table ($cols) VALUES($vals)");
@@ -159,4 +155,193 @@ sub insert_row {
     return eval { $dbh->last_insert_id } || 0;
 }
 
+sub insert_rows {
+    my $self = shift;
+    my ($table, @rows) = @_;
+
+    $self->insert_row($table => $_) for @rows;
+}
+
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Test::FixtureBuilder - Quickly define fixture data for unit tests
+
+=head1 DESCRIPTION
+
+When writing unit tests for applications it is often necessary to load some
+basic data into a database. This data is often referred to fixture data. There
+are several approaches to loading fixture data: Manual, From YAML files, or
+code to create objects.
+
+Sometimes you just want to shove some rows into a database, and you do not want
+to be bothered with the SQL or the object->new calls. In those cases this
+module is for you.
+
+=head1 SYNOPSYS
+
+There are two interfaces to this module.
+
+=head2 DECLARE
+
+The declarative interface is really quite nice.
+
+B<NOTE:> You B<MUST> subclass Test::FixtureBuilder, or use a predefined
+subclass in order to use the declarative form.
+
+    package Test::FixtureBuilder::MyBuilder;
+
+    use DBI;
+    use DBD::SQLite;
+
+    use base Test::FixtureBuilder;
+
+    sub name_to_handle {
+        my $class = shift;
+        my ($name) = @_;
+        return DBI->connect("dbi:SQLite:dbname=$name","","");
+    }
+
+    1;
+
+Then to use it:
+
+    use Test::FixtureBuilder::MyBuilder;
+
+    fixture_db my_db => sub {
+        fixture_table my_table => sub {
+            fixture_row { col1 => 'val1', col2 => 'val2' };
+
+            fixture_row { key => $_, col2 => 'xxx' }
+                for 1 .. 10;
+        };
+
+        fixture_table my_table2 => sub {
+            fixture_row { col1 => 'val1', col2 => 'val2' };
+        };
+    };
+
+    fixture_db my_db2 => sub { ... };
+
+    ...
+
+    1;
+
+=head2 OOP
+
+    use Test::FixtureBuilder ();
+
+    my $fb = Test::FixtureBuilder->new( dbh => $dbh );
+    $fb->insert_row(tableA => { col1 => 'val1' });
+    $fb->insert_row(tableB => { col1 => 'val1' });
+
+    $fb->insert_rows(
+        'tableX',
+        { ... },
+        { ... },
+        ...
+    );
+
+=head1 EXPORTS
+
+=over 4
+
+=item fixture_db db_name => sub { ... }
+
+Create a scope in which fixtures use the db_name database
+
+=item fixture_table table_name => sub { ... }
+
+Create a scope in which fixtures use the table_name table
+
+=item fixture_row { col => val, ... }
+
+=item fixture_row ( col => val, ... )
+
+=item fixture_row col => val, ...
+
+Load a row, you can use a hashref, or key/value pairs.
+
+=item my $meta = $class->FIXTURE_BUILDER_META
+
+Get the meta-object. Documented for completeness, you should not use this
+directly.
+
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item my $dbh = $class->name_to_handle($dbname)
+
+Get a database handle from a name. You must override this before it will do
+anything useful. The default behavior is to die unless the $dbname variable is
+blessed in which case it is returned unchanged.
+
+=item my $fb = $class->new(...)
+
+Create a new instance. Any valid accessor can be specified at construction
+time. This includes accessors for your specific subclass.
+
+=item $class = $fb->class
+
+Used internally.
+
+=item $fb->db($dbname)
+
+=item $dbh = $fb->db
+
+Set the database by name (only useful if you override C<name_to_handle()>).
+When no argument is given it behaves like C<dbh()>.
+
+=item $fb->dbh($dbh)
+
+=item $dbh = $fb->dbh
+
+Get and/or set the database handle.
+
+=item $fb->insert_row(table => { ... })
+
+Insert a row into the specified table of the current database.
+
+=item $fb->insert_rows(table => { ... }, { ... }, ...)
+
+Insert multiple rows into the specified table of the current database.
+
+=back
+
+=head1 AUTHORS
+
+Chad Granum L<exodist7@gmail.com>
+
+=head1 OTHER CREDITS
+
+=over 4
+
+=item DreamHost
+
+I originally developed a tool very similar to this one for use at DreamHost in
+our test suite. DreamHost gave me permission to release an open-source
+implementation of the tool.
+
+L<http://www.dreamhost.com>
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2014 Chad Granum
+
+Test-FixtureBuilder is free software; Standard perl license (GPL and Artistic).
+
+Test-FixtureBuilder is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the license for more details.
+
+=cut
--- t/oo.t
+++ t/oo.t
@@ -40,8 +40,6 @@ BEGIN {
     END { delete $DBs{$_} for keys %DBs }
 }
 
-BEGIN { MyFixtureBuilder->import }
-
 sub fetch {
     my ($table) = @_;
     my $dbh = MyFixtureBuilder->name_to_handle('test') || die "No db";
@@ -51,11 +49,6 @@ sub fetch {
     return $sth->fetchall_arrayref({});
 }
 
-tests can_we_fix_it => sub {
-    my $self = shift;
-    can_ok( $self, qw/fixture_db fixture_table fixture_row/ );
-};
-
 tests full_stack => sub {
     my $self = shift;
 
@@ -63,25 +56,26 @@ tests full_stack => sub {
     is( scalar @{fetch('A')}, 0, "No results A" );
     is( scalar @{fetch('B')}, 0, "No results B" );
 
-    fixture_db test => sub {
-        fixture_table A => sub {
-            fixture_row { a => 1, b => 1 };
-            fixture_row { a => 2, b => 2 };
-            fixture_row { a => 3, b => 3 };
-        };
+    my $builder = MyFixtureBuilder->new( db => 'test' );
 
-        # Assert rows above, but not below
-        is( scalar @{fetch('A')}, 3, "results A" );
-        is( scalar @{fetch('B')}, 0, "Still no results B" );
+    $builder->insert_rows(
+        'A',
+         { a => 1, b => 1 },
+         { a => 2, b => 2 },
+         { a => 3, b => 3 },
+    );
 
-        fixture_table B => sub {
-            fixture_row { a => 1, b => 1 };
-            fixture_row { a => 2, b => 2 };
-            fixture_row { a => 3, b => 3 };
-        };
+    # Assert rows above, but not below
+    is( scalar @{fetch('A')}, 3, "results A" );
+    is( scalar @{fetch('B')}, 0, "Still no results B" );
 
-        is( scalar @{fetch('B')}, 3, "results B" );
-    };
+    $builder->insert_row(B => $_) for (
+         { a => 1, b => 1 },
+         { a => 2, b => 2 },
+         { a => 3, b => 3 },
+    );
+
+    is( scalar @{fetch('B')}, 3, "results B" );
 
     # Assert all rows
     is_deeply(
